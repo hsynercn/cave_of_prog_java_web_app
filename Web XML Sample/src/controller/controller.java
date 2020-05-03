a4e8b85bfe58a64d48e5051ac018ad02070c7a23
hsynercn <huscanercan@gmail.com>
2020-05-03 22:59:59 +0300

    Web xml param example

---
+++ Web XML Sample/src/controller/controller.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class controller
+ */
+@WebServlet("/controller")
+public class controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext servletContext = getServletContext();
+		Integer hits = (Integer)servletContext.getAttribute("hits");
+		if(hits == null) {
+			hits = 0;
+		} else {
+			hits++;
+		}
+		
+		servletContext.setAttribute("hits", hits);
+		PrintWriter out = response.getWriter();
+		out.print("HITS: " + hits);
+		
+		String adminName = servletContext.getInitParameter("admin");
+		out.println("<p>" + adminName + "</p>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
