b75d36b021a740d7acf3f8333de6a94dd78463eb
hsynercn <huscanercan@gmail.com>
2020-05-04 23:58:33 +0300

    JNDI example

---
+++ Databases/src/demo/Datasource.java
@@ -0,0 +1,79 @@
+package demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class Datasource
+ */
+@WebServlet("/Datasource")
+public class Datasource extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private DataSource dataSource;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Datasource() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			InitialContext initContext = new InitialContext();
+			Context env = (Context) initContext.lookup("java:comp/env");
+			dataSource = (DataSource)env.lookup("jdbc/new_schema");
+		} catch (NamingException e) {
+			throw new ServletException();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		try {
+			conn = dataSource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		
+		PrintWriter out = response.getWriter();
+		out.println("Connected to database");
+		
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
